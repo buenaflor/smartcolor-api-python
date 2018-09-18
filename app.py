5224c0a29f6bc582492a4ceb54ea1b0fb346f0ca
Giancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2018-09-18 11:48:37 +0200

    Added GPIO library & routes

--- app.py
+++ app.py
@@ -1,62 +1,63 @@
-# from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request
+import RPi.GPIO as GPIO
 
-# app = Flask(__name__)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-# languages = [{'name': 'Javascript'}, {'name': 'Python'}]
+redPin = 13
+greenPin = 15
+bluePin = 16
 
-
-# @app.route('/', methods=['GET'])
-# def index():
-#     return jsonify({'message': 'it works'})
-
-
-# @app.route('/postman', methods=['POST'])
-# def return_post():
-#     # language = {'name': request.json['name']}
-#     # languages.append(language)
-#     some_var = request.get_json()
-#     return jsonify(languages)
-
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port='3000', debug=True)
-
-from flask import Flask, request
-from flask_restful import Resource, Api
+RGB = [redPin,greenPin,bluePin]
+for pin in RGB:
+    GPIO.setup(pin,GPIO.OUT)
+    GPIO.output(pin,0)
 
 app = Flask(__name__)
-api = Api(app)
 
-power_json = {'on': False}
+def set_rgb_color(red, green, blue):
+    GPIO.output(redPin, red)
+    GPIO.output(greenPin, green)
+    GPIO.output(bluePin, blue)
 
+@app.route('/', methods=['GET'])
+def index():
+    set_rgb_color(1,0,1)
+    return jsonify({'message': 'it works'})
 
-class LEDPower(Resource):
-    def get(self):
-        return {'on': 'true'}
+@app.route('/api/ledpower', methods=['GET'])
+def return_ledstatus():
+    if GPIO.input(redPin) == 0 and GPIO.input(greenPin) == 0 and GPIO.input(bluePin) == 0:
+        return jsonify({'ledOn': False})
+    else:
+        return jsonify({'ledOn': True})
+    
+@app.route('/api/single_color', methods=['POST'])
+def single_color():
+    print("gettin requests")
+    print(request.json['red'])
+    response = {
+            'red': request.json['red'],
+            'green': request.json['green'],
+            'blue': request.json['blue']
+        }
+    red = response['red']
+    green = response['green']
+    blue = response['blue']
+    
+    return jsonify({'successful': green})
 
-    def post(self):
-        power_json = request.get_json()
-        if power_json['on'] == True:
-            power_json['on'] = False
-            print('it is on, turn it off')
-
-            # Turn led off or on here
-            return {'Success': power_json}, 201
-        else:
-            power_json['on'] = True
-            print('it is off, turn it on')
-
-            # Turn led off or on here
-            return {'Success': power_json}, 201
-
-
-class Multi(Resource):
-    def get(self, num):
-        return {'result': num * 10}
-
-
-api.add_resource(LEDPower, '/api/ledpower')
-api.add_resource(Multi, '/multi/<int:num>')
+@app.route('/api/ledpower', methods=['POST'])
+def return_power():
+    print("turning off or on")
+    action = {'ledOn': request.json['ledOn']}
+    ledOn = action['ledOn']
+    if ledOn == True:
+       set_rgb_color(1, 1, 1)
+       return jsonify({'ledOn': True})
+    else:
+       set_rgb_color(0, 0, 0)
+       return jsonify({'ledOn': False})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='3000', debug=True)
+     app.run(host='192.168.0.241', port=3000, debug=True)
